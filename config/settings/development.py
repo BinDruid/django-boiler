190856f0612f6b2892ab3202b0d720bfd3a4c877
BinDruid <abharya.dev@gmail.com>
2022-11-18 21:20:08 +0330

    Add django-compressor app

--- config/settings/development.py
+++ config/settings/development.py
@@ -22,3 +22,5 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "public/")]
+
+COMPRESS_ROOT = os.path.join(BASE_DIR, "public/")
