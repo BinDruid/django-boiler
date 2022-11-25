5be46661499c842caf0a5b3d8dbff92957ea2324
BinDruid <abharya.dev@gmail.com>
2022-11-25 18:23:22 +0330

    Fix media url setting for development enviorment

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 from apps.common.views import handle_erro_404
 
 handler404 = handle_erro_404
@@ -13,3 +15,6 @@ urlpatterns += [
     path("accounts/", include("apps.account.urls")),
     path("", include("apps.common.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
